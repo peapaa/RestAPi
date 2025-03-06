1fff2094fcd1ac382746c568d67a1fdfb8c45952
maidtx <maidtx@newwave.com.vn>
2025-03-06 11:29:04 +0700

    Add project files.

---
+++ Model/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace RestAPi.Model
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
